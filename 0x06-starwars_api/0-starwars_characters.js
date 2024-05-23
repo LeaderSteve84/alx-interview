5da58cfcf8475af7f7d19db7322a25538a28f867
stephen_adah <mailstephenreal@gmail.com>
2024-05-23 09:24:44 +0100

    star war

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+// Star Wars API
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+
+  // the function to fetch and print character names sequentially
+  const fetchCharacterNames = (index) => {
+    if (index >= characters.length) return;
+
+    request(characters[index], (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+        return;
+      }
+      if (charResponse.statusCode !== 200) {
+        console.error(`Failed to fetch character data. Status code: ${charResponse.statusCode}`);
+        return;
+      }
+
+      const characterData = JSON.parse(charBody);
+      console.log(characterData.name);
+      fetchCharacterNames(index + 1);
+    });
+  };
+  fetchCharacterNames(0);
+});
